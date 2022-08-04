95c10cc5af48314415ad1321dd47196a2b1a638a
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-08-04 22:44:40 +0300

    Updated news_init migration with a real News entity structure

--- console/migrations/m220804_192718_news_init.php
+++ console/migrations/m220804_192718_news_init.php
@@ -8,20 +8,27 @@ use yii\db\Migration;
  */
 class m220804_192718_news_init extends Migration
 {
-    const TABLE_NAME = 'news';
+    const TABLE_NAME = '{{%news}}';
 
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
         $this->createTable(self::TABLE_NAME, [
             'id' => $this->primaryKey(),
-            'int' => $this->integer(),
-            'varchar' => $this->string(),
-            'tinyint' => $this->boolean(),
-            'text' => $this->text(),
-        ]);
+            'category_id' => $this->integer(),
+            'slug' => $this->string(255)->notNull()->unique(),
+            'title' => $this->string(255)->notNull(),
+            'description' => $this->text()->notNull(),
+            'enabled' => $this->boolean()->notNull()->defaultValue(0), // 0 or 1
+        ], $tableOptions);
     }
 
     /**
