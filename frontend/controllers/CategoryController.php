68c742469c30afd3ca63c62fc4bc824357a37855
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-08-06 19:02:38 +0300

    Improved category section display on frontend

--- frontend/controllers/CategoryController.php
+++ frontend/controllers/CategoryController.php
@@ -3,9 +3,11 @@
 namespace frontend\controllers;
 
 
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use frontend\models\Category;
+use frontend\models\News;
 
 
 /**
@@ -20,8 +22,16 @@ class CategoryController extends Controller
     {
         $title = 'You are on a page category/index';
 
+        $dataProvider = new ActiveDataProvider([
+            'query' => Category::find()->innerJoinWith('news'),
+            'pagination' => [
+                'pageSize' => 20,
+            ]
+        ]);
+
         return $this->render('index', [
-            'title' => $title
+            'title' => $title,
+            'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -37,8 +47,14 @@ class CategoryController extends Controller
             throw new NotFoundHttpException('Category page was not found.');
         }
 
+        $dataProvider = new ActiveDataProvider([
+            'query' => $model->getNews(),
+            'pagination' => false,
+        ]);
+
         return $this->render('view', [
             'model' => $model,
+            'dataProvider' => $dataProvider,
         ]);
     }
 }
--- frontend/views/category/index.php
+++ frontend/views/category/index.php
@@ -1,12 +1,22 @@
 <?php
 
+use yii\helpers\Html;
+use yii\data\ActiveDataProvider;
+use yii\widgets\ListView;
+
+
 /**
  * @var string $title
+ * @var ActiveDataProvider $dataProvider
  */
-
-use yii\helpers\Html;
-
 
 echo HTML::tag('h2', $title);
 
-echo HTML::a('Go to a view page', ['category/view', 'id' => 19]);
+if ($dataProvider->getCount() > 0) {
+    echo ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemView' => '_category_item',
+        'itemOptions' => ['tag' => null],
+        'options' => ['class' => 'list-group'],
+    ]);
+}
